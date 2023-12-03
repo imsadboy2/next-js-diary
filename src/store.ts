9229e8badca748fdd2c9147ce79bd143260c082f
imsadboy2 <wos961129@gmail.com>
2023-12-03 22:33:50 +0900

    실수한거 수정...ㅠㅠㅠ

--- src/store.ts
+++ src/store.ts
@@ -1,6 +1,17 @@
 import { configureStore, createSlice } from "@reduxjs/toolkit";
 
 
+let feel = createSlice({
+  name: 'feel',
+  initialState : '기분없음',
+  reducers: {
+    changeFeel(state, action){
+      return action.payload
+    }
+  }
+})
+
+
 let imgurl = createSlice({
   name: 'imgurl',
   initialState : '',
@@ -13,8 +24,10 @@ let imgurl = createSlice({
 
 export default configureStore({
   reducer: {
+    feel: feel.reducer,
     imgurl: imgurl.reducer
   }
 })
 
+export let {changeFeel} = feel.actions
 export let {changeimgurl} = imgurl.actions
